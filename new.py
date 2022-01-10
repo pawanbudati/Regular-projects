e695595b145eee4455448ee53db63f15a05026c1
pawanbudati <pawan1122143@gmail.com>
2022-01-10 23:01:51 +0530

    LinkedList

--- new.py
+++ new.py
@@ -1,6 +1,17 @@
-i = input()
-match i:
-    case ("Hello" or "hi"):
-        print("!")
-    case _:
-        print("ghkjf")
+def devideAndRule(input1,n):
+    for i in range(1,n-1):
+        sum = 0
+        for j in range(i):
+            sum += input1[j]
+        sum2 = 0
+        for k in range(i+1,n):
+            sum2 += input1[k]
+        if sum==sum2:
+            return i
+    else:
+        return 0
+
+
+i = int(input())
+s = list(map(int,input().split()))
+print(devideAndRule(s,i))
